715a751c65f70302b2f94ceb15e1ea2e2c6e5d3f
Netanel Berman <njberman16@hotmail.com>
2024-07-23 12:48:51 +0100

    fjirst

---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+function ColorMyPencils(color)
+  color = color or 'tokyonight-night'
+  vim.cmd.colorscheme(color)
+  vim.o.background = 'dark'
+
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end

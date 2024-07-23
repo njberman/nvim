d7aafdc22822c30bffb66e78948a786eb05a08e7
njberman <njberman16@hotmail.com>
2024-07-23 21:09:17 +0100

    add keymaps.lua, but currently no keymaps set

--- init.lua
+++ init.lua
@@ -844,6 +844,9 @@ require('lazy').setup({
     },
   },
 })
+
+require 'custom.keymaps'
+
 -- inoremap jj <ESC>
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>print', function()
+  print 'Hello world!'
+end)

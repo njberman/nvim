e2a8aad82f1ff23c7329c24b10e6a3a49338e10a
njberman <njberman16@hotmail.com>
2024-07-25 19:10:11 +0100

    fix gg and G not working so well

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', 'gg', 'gg0')
-vim.keymap.set('n', 'G', 'G$')
+vim.keymap.set({ 'n', 'v' }, 'gg', 'gg0')
+vim.keymap.set({ 'n', 'v' }, 'G', 'G$')
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<C-p>', ':Ex\n')

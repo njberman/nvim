361a30a2fff701aa1d2e5f2beb6869596d615db0
njberman <njberman16@hotmail.com>
2024-07-25 22:39:09 +0100

    add some of the primeagens keybinds

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -1,4 +1,19 @@
 vim.keymap.set({ 'n', 'v' }, 'gg', 'gg0')
 vim.keymap.set({ 'n', 'v' }, 'G', 'G$')
+
 vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('n', '<C-p>', ':Ex\n')
+
+vim.keymap.set('n', '<C-p>', vim.cmd.Ex)
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])

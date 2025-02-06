f59b86f421bf40bd621761ee7908b98524ac1c33
njberman <njberman16@hotmail.com>
2025-02-06 20:21:04 +0000

    comment out vimtex

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,13 +15,13 @@ return {
     dependencieds = { 'nvim-lua/plenary.nvim' },
   },
   { 'ThePrimeagen/vim-be-good' },
-  {
-    'lervag/vimtex',
-    lazy = false, -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
-    init = function()
-      -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = 'zathura'
-    end,
-  },
+  -- {
+  --   'lervag/vimtex',
+  --   lazy = false, -- we don't want to lazy load VimTeX
+  --   -- tag = "v2.15", -- uncomment to pin to a specific release
+  --   init = function()
+  --     -- VimTeX configuration goes here, e.g.
+  --     vim.g.vimtex_view_method = 'zathura'
+  --   end,
+  -- },
 }

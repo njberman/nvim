2170e0a8ab882364c3329f36571ca430f447441f
njberman <njberman16@hotmail.com>
2025-08-06 19:41:22 +0100

    tryna make search windows nicer; didn't really work

--- init.lua
+++ init.lua
@@ -302,7 +302,9 @@ require('lazy').setup({
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {})
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 0,
+        })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
       -- It's also possible to pass additional configuration options.
